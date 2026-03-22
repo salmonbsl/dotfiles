ee8663d9ec59f8596369ee5cd590d293bbc50b41
salmonbsl <main@salmonbsl.net>
2026-03-22 19:51:39 +0900

    feat: add termite.nvim

--- dot_config/nvim/lua/plugins/terminal.lua
+++ dot_config/nvim/lua/plugins/terminal.lua
@@ -1,61 +1,77 @@
 return {
+	-- {
+	-- 	"https://github.com/akinsho/toggleterm.nvim",
+	-- 	event = "VeryLazy",
+	-- 	config = true,
+	-- 	keys = function()
+	-- 		local keys = {}
+	--
+	-- 		-- 移行のため一時的に設定
+	-- 		-- 使わなくなったら消す
+	-- 		for i = 0, 9 do
+	-- 			table.insert(keys, {
+	-- 				"<A-" .. i .. ">",
+	-- 				function()
+	-- 					vim.cmd("90" .. i .. "ToggleTerm direction=float")
+	-- 				end,
+	-- 				mode = { "n", "i", "x", "t" },
+	-- 				desc = "Toggle Term " .. i,
+	-- 			})
+	-- 		end
+	--
+	-- 		table.insert(keys, {
+	-- 			"<leader>ta",
+	-- 			function()
+	-- 				vim.cmd("TermNew")
+	-- 			end,
+	-- 			mode = { "n" },
+	-- 			desc = "[a]dd New Terminal",
+	-- 		})
+	--
+	-- 		table.insert(keys, {
+	-- 			"<leader>tt",
+	-- 			function()
+	-- 				vim.cmd("ToggleTermToggleAll")
+	-- 			end,
+	-- 			mode = { "n" },
+	-- 			desc = "[t]oggle All Terminals",
+	-- 		})
+	--
+	-- 		table.insert(keys, {
+	-- 			"<C-'>",
+	-- 			function()
+	-- 				vim.cmd("ToggleTermToggleAll")
+	-- 			end,
+	-- 			mode = { "n", "t" },
+	-- 			desc = "Toggle All Terminals",
+	-- 		})
+	--
+	-- 		table.insert(keys, {
+	-- 			"<leader>tf",
+	-- 			function()
+	-- 				vim.cmd("999ToggleTerm direction=float")
+	-- 			end,
+	-- 			mode = { "n" },
+	-- 			desc = "Open [f]loat Terminal",
+	-- 		})
+	--
+	-- 		return keys
+	-- 	end,
+	-- },
 	{
-		"https://github.com/akinsho/toggleterm.nvim",
-		event = "VeryLazy",
-		config = true,
-		keys = function()
-			local keys = {}
-
-			-- 移行のため一時的に設定
-			-- 使わなくなったら消す
-			for i = 0, 9 do
-				table.insert(keys, {
-					"<A-" .. i .. ">",
-					function()
-						vim.cmd("90" .. i .. "ToggleTerm direction=float")
-					end,
-					mode = { "n", "i", "x", "t" },
-					desc = "Toggle Term " .. i,
-				})
-			end
-
-			table.insert(keys, {
-				"<leader>ta",
-				function()
-					vim.cmd("TermNew")
-				end,
-				mode = { "n" },
-				desc = "[a]dd New Terminal",
-			})
-
-			table.insert(keys, {
-				"<leader>tt",
-				function()
-					vim.cmd("ToggleTermToggleAll")
-				end,
-				mode = { "n" },
-				desc = "[t]oggle All Terminals",
-			})
-
-			table.insert(keys, {
-				"<C-'>",
-				function()
-					vim.cmd("ToggleTermToggleAll")
-				end,
-				mode = { "n", "t" },
-				desc = "Toggle All Terminals",
-			})
-
-			table.insert(keys, {
-				"<leader>tf",
-				function()
-					vim.cmd("999ToggleTerm direction=float")
-				end,
-				mode = { "n" },
-				desc = "Open [f]loat Terminal",
-			})
-
-			return keys
-		end,
+		"https://github.com/ruicsh/termite.nvim",
+		opts = {
+			position = "bottom",
+			keymaps = {
+				toggle = "<C-\\>",
+				create = "<C-t>",
+				next = "<C-]>",
+				prev = "<C-[>",
+				normal_mode = false,
+				focus_editor = false,
+				maximize = "<C-z>",
+				close = "q",
+			},
+		},
 	},
 }
